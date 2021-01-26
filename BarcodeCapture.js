ce29bc25d2fe78777303811429ec8798aba1438d
Vasily Asakasinsky <asakasinsky@gmail.com>
2021-01-26 17:56:37 +0300

    Кроссбраузерность
    на событии keyup разные браузеры передают разное значение which.
    Интересное чтиво:
    [JavaScript Madness: Keyboard Events page by Jan Wolter](https://unixpapa.com/js/key.html)

--- BarcodeCapture.js
+++ BarcodeCapture.js
@@ -72,28 +72,46 @@ var BarcodeCapture = (function(document) {
             }));
     }
 
+    BarcodeCapture.prototype.parseTabSequence = function(seq) {
+        var res = [];
+        var code = [];
+        var idx = 0;
+        var seqLen = seq.length;
+        while (idx < seqLen) {
+            if (code.length >= 3) {
+                if (seq[idx] === '0' && seq[idx + 1] !== '0') {
+                    res.push(code.join(''));
+                    code = [];
+                }
+            }
+            code.push(seq[idx]);
+            idx++;
+        }
+        res.push(code.join(''));
+        return res;
+    };
+
     BarcodeCapture.prototype.tabSequence2barcode = function(sequence) {
         // Вводные:
         // - последовательность кратна четырём (один ALT-Code)
         // - каждое число последовательности — ASCII-код символа
         // - каждый символ — часть числа
-        // - в числе по три разряда
-        // - после каждого числа идёт ASCII-код символа ALT
-        // - трёхразрядное число - ASCII-код одного символа из barcode
+        // - в числе по три или четыре разряда, первый всегда ноль
+        // - число - ASCII-код одного символа из barcode
         //
         // Для того, чтобы передать barcode 4607027767471, понадобится 13*4 ASCII-кодов.
         // Такая заморока нужна для того, чтобы в режиме эмуляции USB-HID клавиатуры
         // отсутствовало влияние текущей раскладки и передавались непечатные символы,
         // в частности, разделитель GS (код 29)
         // В инструкции к сканнеру этот режим называется «ALT+Number output»
-        sequence = String.fromCharCode.apply(this, sequence);
-        sequence = sequence.split('');
+        sequence = String.fromCharCode.apply(this, sequence)
+        sequence = this.parseTabSequence(sequence);
         // В конце последовательности ожидаем три символа:
         // - CR (13) — возврат каретки
         // - LF (10) — перевод строки
         // - NULL (пусто)
-        var controlSuffix = sequence.splice(Math.max(sequence.length - 3, 1))
-        if (controlSuffix.join('-') === '013-010-') {
+        var controlSuffix = sequence.splice(Math.max(sequence.length - 2, 1))
+        if (controlSuffix.join('-') === '013-010') {
             return String.fromCharCode.apply(this, sequence);
         }
         return null;
@@ -123,7 +141,7 @@ var BarcodeCapture = (function(document) {
             return;
         }
         this.captureStarted = true;
-        document.addEventListener('keyup', this.keyUpHandler, false);
+        document.addEventListener('keypress', this.keyUpHandler, false);
 
         if (this.options.debug) {
             console.log('barcode:  -- Capture Sequence Started --');
@@ -135,7 +153,7 @@ var BarcodeCapture = (function(document) {
             return;
         }
         this.captureStarted = false;
-        document.removeEventListener('keyup', this.keyUpHandler, false);
+        document.removeEventListener('keypress', this.keyUpHandler, false);
         if (this.options.debug) {
             console.log('barcode:  -- Capture Sequence Stopped --');
         }
@@ -172,6 +190,7 @@ var BarcodeCapture = (function(document) {
         if (this.watcher) {
             this.clearWatcher();
         }
+
         this._buffer.push(e.which);
         this.setWatcher();
     };
@@ -185,21 +204,20 @@ var BarcodeCapture = (function(document) {
             event.target.tagName.toLowerCase()
         ) !== -1;
     };
-  
-    BarcodeCapture.prototype.test = function (altCodeSsequence) {
+
+    BarcodeCapture.prototype.test = function(altCodeSsequence) {
         var body = document.body;
         var evt;
         var code;
         for (let i = 0; i < altCodeSsequence.length; i++) {
             code = altCodeSsequence[i].charCodeAt(0)
-            evt = new KeyboardEvent('keyup', {
-                'bubbles':true, 
-                'keyCode':code, 
-                'which':code
-            }); 
-          body.dispatchEvent(evt);
+            evt = new KeyboardEvent('keypress', {
+                'bubbles': true,
+                'keyCode': code,
+                'which': code
+            });
+            body.dispatchEvent(evt);
         }
     }
-
     return BarcodeCapture;
 })(document);
